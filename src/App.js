972320ca2e32c3d1bd1dc0d7778bc9484914976b
sofiagschwend <37657968+sofiagschwend@users.noreply.github.com>
2021-04-25 14:50:38 +0300

    Basic GUI and npm install use-puchdb
    Basic GUI and npm install use-puchdb

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+function myFunction(x) {
+  x.classList.toggle("change");
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>hallo</div>
-      </header>
       <body>
-        <div>hallo</div>
+        <h1>Rallye Timer</h1>
+        <div class="container" onlick="myFunction(this)">
+  <div class="bar1"></div>
+  <div class="bar2"></div>
+  <div class="bar3"></div>
+</div>
+
+        <table>
+          <tbody>
+            <tr>
+              <th className="th">Durchlaufzeit</th>
+              <th>Differenz</th>
+              <th>Intervall</th>
+              <th>Abweichung</th>
+            </tr>
+            <tr>
+              <td>Wert 1</td>
+              <td>Wert 2</td>
+              <td>Wert 3</td>
+              <td>Wert 4</td>
+            </tr>
+          </tbody>
+        </table>
+      
       </body>
-    </div>
   );
 }
 
